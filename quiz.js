e367e6b198c03d304271ec7dbfc9390c1660a85e
morpole <40895085+morpole@users.noreply.github.com>
2020-04-28 23:37:20 +0100

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -5,7 +5,7 @@
         document.getElementById("image").src = "goodjob.png"; 
         document.getElementById("code").innerHTML = "VoucherCode: BNY-UNH-IUJ-PLO";  
         window.scroll({
-            top: 100,
+            top: 160,
             left: 0,
             behavior: 'smooth'
           });
