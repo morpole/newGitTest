e0baa46176497f8392837fd6345736bd92d6bf9b
morpole <40895085+morpole@users.noreply.github.com>
2020-04-28 17:35:21 +0100

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,11 @@
+
+
+    function ShowRedo() 
+{
+        document.getElementById("image").src = "goodjob.png";  
+    }
+
+    function ShowUndo() 
+{
+        document.getElementById("image").src = "nope.gif"; 
+    }
