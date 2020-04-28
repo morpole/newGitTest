8b637db4aa772dc7ba5ac9b9bbc0264a18725a81
morpole <40895085+morpole@users.noreply.github.com>
2020-04-29 00:57:02 +0100

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@
 
     function ShowRedo() 
 {
-        document.getElementById("image").src = "goodjob.png"; 
+        document.getElementById("image").src = "image5.jpeg"; 
         document.getElementById("code").innerHTML = "VoucherCode: BNY-UNH-IUJ-PLO";  
         window.scroll({
             top: 250,
@@ -22,5 +22,44 @@
           });
     }
 
-    
+    function ShowUndo1() 
+    {
+            document.getElementById("image").src = "image1.jpeg"; 
+            document.getElementById("code").innerHTML = "";
+            window.scroll({
+                top: 100,
+                left: 0,
+                behavior: 'smooth'
+              });
+        }
 
+        function ShowUndo2() 
+        {
+                document.getElementById("image").src = "image2.jpeg"; 
+                document.getElementById("code").innerHTML = "";
+                window.scroll({
+                    top: 100,
+                    left: 0,
+                    behavior: 'smooth'
+                  });
+            }
+            function ShowUndo3() 
+        {
+                document.getElementById("image").src = "image3.jpeg"; 
+                document.getElementById("code").innerHTML = "";
+                window.scroll({
+                    top: 100,
+                    left: 0,
+                    behavior: 'smooth'
+                  });
+            }
+            function ShowUndo4() 
+            {
+                    document.getElementById("image").src = "image4.jpeg"; 
+                    document.getElementById("code").innerHTML = "";
+                    window.scroll({
+                        top: 100,
+                        left: 0,
+                        behavior: 'smooth'
+                      });
+                }
