7b2dc0c1f67ed210768c5c467d970b96f59d0da5
morpole <40895085+morpole@users.noreply.github.com>
2020-04-28 20:50:13 +0100

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -3,18 +3,23 @@
     function ShowRedo() 
 {
         document.getElementById("image").src = "goodjob.png"; 
-        document.getElementById("code").innerHTML = "VoucherCode: BNY-UNH-IUJ-PLO";
+        document.getElementById("code").innerHTML = "VoucherCode: BNY-UNH-IUJ-PLO";  
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+          });
     }
 
     function ShowUndo() 
 {
         document.getElementById("image").src = "nope.gif"; 
         document.getElementById("code").innerHTML = "";
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+          });
     }
 
-
-/*<script>
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello World";
-}
-</script>*/
+    
